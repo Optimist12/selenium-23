de766cd5c324bf6b9f5e03c35db0125b7a96429d
Aleksandr Stepuchev <optimist.demos@gmail.com>
2018-11-30 01:52:27 +0300

    fix add duck to basket

--- src/test/java/AddDuckToBasket.java
+++ src/test/java/AddDuckToBasket.java
@@ -1,11 +1,11 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
 
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AddDuckToBasket {
@@ -16,36 +16,32 @@ public class AddDuckToBasket {
         for (int i = 1; i <= 3; i++) {
             String num = String.valueOf(i);
             open("http://localhost/litecart/public_html/");
-            $("#box-most-popular li:nth-child(1)").click();
+            ElementsCollection duck = $$(".product.column.shadow.hover-light");
+            duck.get(0).click();
+//            $("#box-most-popular li:nth-child(1)").click();
             if ($(By.tagName("h1")).getAttribute("textContent").equals("Yellow Duck"))
                 new Select($("[name='options[Size]']")).selectByValue("Large");
             $(".quantity button").click();
             $("#cart .quantity").waitUntil((Condition.text(num)), 5000);
+            open("http://localhost/litecart/public_html/");
 
         }
         $("#cart .link").click();
         ElementsCollection row = $$(By.cssSelector("#order_confirmation-wrapper tr td.item"));
         for (int i = 0; i <= row.size()  ; i++) {
             ElementsCollection shortcuts = $$(By.cssSelector("#box-checkout-cart .shortcuts li"));
+            SelenideElement empty = $(withText("There are no items in your cart"));
             if (shortcuts.size() > 0) {
                 SelenideElement shortcut = $("#box-checkout-cart .shortcuts li:nth-child(1)");
                 shortcut.click();
                 $(By.xpath("//button[@value='Remove']")).click();
-
+                row.get(i).waitUntil(Condition.not(Condition.visible), 10000);
             }
-            else
-            {
+            else {
                 $(By.xpath("//button[@value='Remove']")).click();
-                row.get(0).waitUntil(Condition.not(Condition.visible), 5000);
-
+                empty.shouldBe(Condition.visible);
             }
-
         }
-        row.get(0).waitUntil(Condition.not(Condition.visible), 5000);
-
-        Assert.assertEquals($("#checkout-cart-wrapper p").getText(),
-                "There are no items in your cart.");
-
 
     }
 }
